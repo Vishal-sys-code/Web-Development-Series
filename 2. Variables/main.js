87e90c759ddd3bd42fdeb9086270590bc8375373
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-10 16:01:58 +0530

    Update main.js

---
+++ 2. Variables/main.js
@@ -0,0 +1,13 @@
+/*
+Variables: A container that stores a value. It behaves as if it were the value it contains.
+
+1. Declaration      let x;
+2. Assignment       x= 10;
+*/
+
+let x; // Declaration
+x = 100; // Assignment
+console.log(x);
+
+let y = 200; // Declaration and Assignment
+console.log(y);
