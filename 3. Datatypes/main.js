f564118189b4457a0bb4bd559c1ce2318c50488d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-10 16:10:00 +0530

    added new js

---
+++ 3. Datatypes/main.js
@@ -0,0 +1,30 @@
+let age = 25;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 3;
+console.log(gpa);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+console.log(typeof age);
+console.log(typeof gpa);
+console.log(typeof price);
+
+let firstName = "Vishal";
+let favFood = "Pizza";
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`Your Favorite food is: ${favFood}`);
+
+let online = true;
+console.log(typeof online);
+console.log(`Vishal is online: ${online}`);
+
+let forSale = true;
+console.log(typeof forSale);
+console.log(`Is this your car for sale: ${forSale}`);
