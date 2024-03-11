34f6d5b7cd89ea497725849b49a63e3205e58561
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 18:46:20 +0530

    Update main.js

--- 10. Random Number Generator/main.js
+++ 10. Random Number Generator/main.js
@@ -1,10 +1,18 @@
 const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
+const myLabel1 = document.getElementById("myLabel1");
+const myLabel2 = document.getElementById("myLabel2");
+const myLabel3 = document.getElementById("myLabel3");
 const min = 1;
 const max = 6;
-let randNum;
+let randNum1;
+let randNum2;
+let randNum3;
 
 myButton.onclick = function(){
-    randNum = Math.floor(Math.random() * max) * min;
-    myLabel.textContent = randNum;
+    randNum1 = Math.floor(Math.random() * max) * min;
+    randNum2 = Math.floor(Math.random() * max) * min;
+    randNum3 = Math.floor(Math.random() * max) * min;
+    myLabel1.textContent = randNum1;
+    myLabel2.textContent = randNum2;
+    myLabel3.textContent = randNum3;
 }
