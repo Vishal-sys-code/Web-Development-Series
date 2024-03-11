fcdac333da0d30eb2e277c05b69f06682893f94d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 08:29:15 +0530

    Update main.js

---
+++ 5. User Input in JS/main.js
@@ -0,0 +1,12 @@
+/*
+ACCEPTING THE USER INPUT IN JAVASCRIPT HAS 2 METHODS:
+    1. Easy Way: Window Prompt
+    2. Professional Way: HTML Textbox
+*/
+
+// Window Prompt User Input
+/*
+let username;
+username = window.prompt("What's your username?");
+console.log(username);
+*/
