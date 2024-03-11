aac6aad9c8c09887a07a8401ab9bdf55d23c2fa8
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 17:03:20 +0530

    Update main.js

--- 9. Maths in JS/main.js
+++ 9. Maths in JS/main.js
@@ -2,3 +2,21 @@
 ------------- MATHEMATICS IN JAVASCRIPT -------------
 Built in Object that provides a collection of properties and methods
 */
+
+console.log(`The value of PI is: ${Math.PI}`);
+console.log(`The value of E is: ${Math.E}`);
+
+
+let x = 3.99;
+let y = 2.13;
+let z;
+
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.round(x));
+
+console.log(Math.floor(y));
+console.log(Math.ceil(y));
+console.log(Math.round(y));
+
+console.log(z);
