8464f7e28658efddf70f2ca27e63f0d4029fa82b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 10:31:56 +0530

    Update main.js

--- 6. Type Conversions/main.js
+++ 6. Type Conversions/main.js
@@ -3,5 +3,21 @@ Type Conversion: Change the datatype of a value to another (Strings, Numbers, Bo
 */
 
 let age = window.prompt("How old are you?");
+age = Number(age);
 age+=1;
-console.log(age);
+console.log(`The age is: ${age}`);
+console.log(`The type of variable age is: ${typeof age}`);
+
+// ----------------------------------------------------------------------------------------------
+
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
