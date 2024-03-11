975bd8b32491ef0f805244954f412c3c8a91dd0d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 10:53:09 +0530

    Update main.js

--- 7. Constants/main.js
+++ 7. Constants/main.js
@@ -2,10 +2,22 @@
 CONSTANTS: A variable that can't be changed.
 */
 
-let pi = 3.14159;
+let PI = 3.14159;
 let radius;
 let circumference;
+
+/* USER INPUT THROUGH THE WINDOW PROMPT */
+
+/*
 radius = window.prompt("Enter the radius of a circle");
 radius = Number(radius);
-circumference = 2 * pi * radius;
+circumference = 2 * PI * radius;
 console.log(circumference);
+*/
+
+document.getElementById("mySubmit").onclick = function(){
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference;
+}
