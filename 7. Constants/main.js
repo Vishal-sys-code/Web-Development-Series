a89ced157e19368ce4bb0364d0baa825e5c7b12b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-11 16:10:46 +0530

    Update main.js

--- 7. Constants/main.js
+++ 7. Constants/main.js
@@ -19,5 +19,5 @@ document.getElementById("mySubmit").onclick = function(){
     radius = document.getElementById("myText").value;
     radius = Number(radius);
     circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = circumference;
+    document.getElementById("myH3").textContent = circumference + " cm";
 }
