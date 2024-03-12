fae1017a301f1df024aa69f3c5d43d61873974ff
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 19:00:10 +0530

    Update main.js

--- 18. Logical Operator/main.js
+++ 18. Logical Operator/main.js
@@ -6,3 +6,20 @@ OR = ||
 NOT = !
 */
 
+const temp = 20;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD");
+}
+
+const temp1 = 20;
+
+if(temp1 > 0 || temp1 <= 30){
+    console.log("The weather is GOOD");
+}
+else{
+    console.log("The weather is BAD");
+}
