4b4487377e3be08ebc0dbd86f494a0720cd97b15
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 14:59:12 +0530

    Update main.js

---
+++ 17. Method Chaining/main.js
@@ -0,0 +1,10 @@
+/*
+Method Chaining: Calling one method after another in one continuous of code.
+*/
+
+// ------------------ NO METHOD CHAINING ------------------
+
+
+
+
+// ------------------ METHOD CHAINING ------------------
