75a175b1d8536d1682906514df4a7fc5ac5874ea
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 10:14:13 +0530

    Update main.js

--- 13. Ternary Operator/main.js
+++ 13. Ternary Operator/main.js
@@ -3,3 +3,19 @@ Ternary Operator: a shortcut to if() and else{} statements, helps to assign a va
 
 condition ? Code_IF_TRUE : Code_IF_FALSE;
 */
+
+// let age = 21;
+// let message = age >= 18 ? "You're an adult" : "You're a minor";
+// console.log(message);
+
+// let time = 9;
+// let greeting = time < 12 ? "Good Morning!" : "Good Afternoon!";
+// console.log(greeting);
+
+// let isStudent = false;
+// let message = isStudent ? "You are a student" : "You are not a student";
+// console.log(message);
+
+// let purchaseAmount = 99;
+// let discount = purchaseAmount >= 100 ? 10 : 0;
+// console.log(`Your total is: $${purchaseAmount - purchaseAmount * (discount)}`);
