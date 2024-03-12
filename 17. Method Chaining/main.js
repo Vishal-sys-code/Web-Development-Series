51c6535cc1907643ccc5558f850c2f96e7312d51
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 15:02:55 +0530

    Update main.js

--- 17. Method Chaining/main.js
+++ 17. Method Chaining/main.js
@@ -4,7 +4,15 @@ Method Chaining: Calling one method after another in one continuous of code.
 
 // ------------------ NO METHOD CHAINING ------------------
 
+let username = window.prompt("Enter your username: ");
+username = username.trim();
+let letter = username.charAt(3);
+letter = letter.toUpperCase();
 
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
 
+console.log(username);
 
 // ------------------ METHOD CHAINING ------------------
