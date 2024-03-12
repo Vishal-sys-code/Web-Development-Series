2687ba335c637ebb0a457b97913667c551cf35ee
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 08:14:37 +0530

    Update main.js

---
+++ 11. Conditional Statements/main.js
@@ -0,0 +1,11 @@
+/* 
+IF STATEMENTS: If a condition is true, execute some code if not then do something else.
+*/
+
+let age = 25;
+if(age >= 18){
+    console.log("Your are old enough to enter this site");
+}
+else{
+    console.log("Your must be 18+ to enter this site");
+}
