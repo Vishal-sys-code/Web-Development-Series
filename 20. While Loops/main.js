e3318544292d91313afa89a7b8094fd09652e8f6
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 20:19:17 +0530

    Update main.js

--- 20. While Loops/main.js
+++ 20. While Loops/main.js
@@ -25,3 +25,14 @@ console.log(`Hello ${username1}`);
 // Output: Hello Vishal Pandey
 
 // --------------------------------------------------------------------------------------------------------------
+
+let username2 = "";
+while(username2 === ""){
+    username2 = window.prompt(`Enter Your Name`);
+}
+console.log(`Hello ${username2}`);
+
+// In this upper part of code the user must enter the username into the prompt and the output will be the Hello ${username}. The thing is when the prompt is being cancelled then the output is the Hello null. So, for taking the input from the user we need to take the input aggressively. So for that we need to add a condition of the username === null. Let's see into the next part of the code.
+
+// --------------------------------------------------------------------------------------------------------------
+
