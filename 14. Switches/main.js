4277a530ce3bf7bd458efdea2ed7ff5bf38dbeda
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 10:20:53 +0530

    Update main.js

--- 14. Switches/main.js
+++ 14. Switches/main.js
@@ -1,3 +1,32 @@
 /*
 Switch: It can be an efficient replacement to many else-if statements
 */
+
+let day = 1;
+switch(day){
+    case 1:
+        console.log("It is Monday");
+        break;
+    case 2:
+        console.log("It is Tueday");
+        break;
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thrusday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7: 
+        console.log("It is Sunday");
+        break;
+    default:
+        console.log("Invalid Day!");
+}   
+
+// ------------------------------------------------------------------
