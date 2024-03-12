53485cfaa33aee8f95254adaf838a9985a925718
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 20:13:57 +0530

    Update main.js

---
+++ 20. While Loops/main.js
@@ -0,0 +1,19 @@
+/*
+While Loop: Repeat some code WHILE some condition is true. 
+*/
+
+/*
+let username = "";
+while(username === ""){
+    console.log("You didn't enter your name");
+}
+console.log(`Hello ${username}`);
+*/
+
+//This upper program will print the message log of "You didn't enter your name" upto infinity.
+
+let username1 = "Vishal Pandey";
+while(username1 === ""){
+    console.log("You didn't enter your name");
+}
+console.log(`Hello ${username1}`);
