e9db4403cdb80630fa4feadfabda0f261f49723d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 14:12:11 +0530

    Update main.js

--- 15. String Methods/main.js
+++ 15. String Methods/main.js
@@ -1,2 +1,14 @@
 // String Methods: It allows you to manipulate and work with text (strings)
 
+let username = "Vishal Pandey";
+
+console.log(username.charAt(0));
+console.log(username.charAt(3));
+
+console.log(username.indexOf("P"));
+console.log(username.indexOf(" "));
+console.log(username.indexOf("l"));
+
+console.log(username.lastIndexOf("P"));
+console.log(username.lastIndexOf(" "));
+console.log(username.lastIndexOf("h"));
