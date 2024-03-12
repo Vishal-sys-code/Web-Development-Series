4891f3b6d0e9ea988afab0a55e76260eb31c5f4d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 19:59:29 +0530

    Update main.js

---
+++ 19. Strict Equality/main.js
@@ -0,0 +1,8 @@
+/*
+    OPERATOR        MEANING
+    =           Assignment Operator
+    ==          Comparision Operator (compare if values are equal)
+    ===         Strict Equality Operator (compare if values & datatype are equal)
+    !=          Inequality operator
+    !==         Strict inequality operator
+*/
