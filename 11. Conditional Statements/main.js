9856b95be03d1673b11f9a94518a884bdad5da6d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 08:50:44 +0530

    Update main.js

--- 11. Conditional Statements/main.js
+++ 11. Conditional Statements/main.js
@@ -9,3 +9,44 @@ if(age >= 18){
 else{
     console.log("Your must be 18+ to enter this site");
 }
+
+// -------------------------------------------------------------------------
+
+let time = 12;
+if(time < 12){
+    console.log("Good Morning!");
+}
+else{
+    console.log("Good Afternoon!");
+}
+
+// -------------------------------------------------------------------------
+
+let isStudent = false;
+if(isStudent){
+    console.log("Your are a student");
+}
+else{
+    console.log("Your are NOT a student");
+}
+
+// -------------------------------------------------------------------------
+
+let age_1 = 25;
+let hasLicense = false;
+
+if(age >= 16){
+    console.log("You are old enough to drive");
+    if(hasLicense){
+        console.log("You have your license!"); 
+    }
+    else{
+        console.log("Your do not have your license yet!");
+    }
+}
+else{
+    console.log("Your must be 16+ to have a license");
+}
+
+// -------------------------------------------------------------------------
+
