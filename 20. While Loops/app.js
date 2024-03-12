51a5fd45cbed7e809fb0dbe9a1a5295a3875040b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-12 22:58:51 +0530

    Update app.js

--- 20. While Loops/app.js
+++ 20. While Loops/app.js
@@ -1,5 +1,13 @@
-myUsername = document.getElementById("myUsername");
-myPassword = document.getElementById("myPassword");
 mySubmit = document.getElementById("mySubmit");
 myText = document.getElementById("myText");
 
+mySubmit.onclick = function(){
+    myUsername = document.getElementById("myUsername").value;
+    myPassword = document.getElementById("myPassword").value;
+    if(myUsername == "thisisbillgates" && myPassword == "Bill@2011"){
+        
+    }
+    else if(){
+
+    }
+}
