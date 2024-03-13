920d58d4c681d3c9844e023d90a48a9363cf1f3c
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 17:37:18 +0530

    Update main.js

--- 26. Arrays/main.js
+++ 26. Arrays/main.js
@@ -9,3 +9,12 @@ console.log(fruits[0]);
 console.log(fruits[1]);
 console.log(fruits[2]);
 console.log(fruits[3]);
+
+fruits[3] = "coconut";
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.unshift("mango");
+console.log(fruits);
