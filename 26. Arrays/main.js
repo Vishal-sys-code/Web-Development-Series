8a3d367111d00ea0eb94cd326d3ce7fcd3b8d39e
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 18:09:34 +0530

    Update main.js

--- 26. Arrays/main.js
+++ 26. Arrays/main.js
@@ -18,60 +18,59 @@ orange
 banana
 undefined
 */
-
 // ----------------------------------------------------------------------------
 
 console.log("----------------------------");
-console.log("Adding the element at the last index");
+console.log("Adding the element at the last index: ");
 fruits[3] = "coconut";
 console.log(fruits);
 
 console.log("----------------------------");
-console.log("Popping Statement");
+console.log("Popping Statement: ");
 fruits.pop();
 console.log(fruits);
 
 console.log("----------------------------");
-console.log("Unshift Method");
+console.log("Unshift Method: ");
 fruits.unshift("mango");
 console.log(fruits);
 
 console.log("----------------------------");
-console.log("Shift Method");
+console.log("Shift Method: ");
 fruits.shift();
 console.log(fruits);
 
 console.log("----------------------------");
-console.log("Length of the array");
+console.log("Length of the array: ");
 let len_fruits = fruits.length;
 console.log(len_fruits);
 
 console.log("----------------------------");
-console.log("indexOf Method");
+console.log("indexOf Method: ");
 let index = fruits.indexOf("banana");
 console.log(index);
 
 console.log("----------------------------");
-console.log("Traversing the array using the for-loops");
+console.log("Traversing the array using the for-loops: ");
 for(let i = 0; i < len_fruits; i++){
     console.log(fruits[i]);
 }
 
 console.log("----------------------------");
-console.log("Traversing the array using the for-each loops");
+console.log("Traversing the array using the for-each loops: ");
 for(let fruit of fruits){
     console.log(fruit);
 }
 
 console.log("----------------------------");
-console.log("Sorting the array");
+console.log("Sorting the array: ");
 fruits.sort();
 for(let fruit of fruits){
     console.log(fruit);
 }
 
 console.log("----------------------------");
-console.log("Reversing the array");
+console.log("Reversing the array: ");
 fruits.reverse();
 for(let fruit of fruits){
     console.log(fruit);
