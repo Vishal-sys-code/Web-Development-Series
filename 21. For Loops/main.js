2640b529aa2cfc804326ad9cf59069472bd28820
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 09:14:16 +0530

    Create main.js

---
+++ 21. For Loops/main.js
@@ -0,0 +1,23 @@
+/*
+For Loops: Repeat some code a LIMITED amount of times.
+*/
+
+/*
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+}
+*/
+// in the uppar part of the code will print the 10 numbers.
+
+/*
+for(let i = 1; i <= 20; i++){
+    if(i == 13){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
+*/
+
+// In this part of the code which is written above will skip the number = 13.
