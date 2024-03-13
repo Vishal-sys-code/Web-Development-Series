217294c85fbac69823b7e6823db47d265431e52f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 16:12:25 +0530

    Update main.js

--- 25. Temperature Conversion/main.js
+++ 25. Temperature Conversion/main.js
@@ -3,6 +3,20 @@ const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelcius = document.getElementById("toCelcius");
 const myResult = document.getElementById("myResult");
 
-function convert(){
+let temp;
 
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        myResult.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(toCelcius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5 / 9);
+        myResult.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        myResult.textContent = "Select a unit";
+    }
 }
