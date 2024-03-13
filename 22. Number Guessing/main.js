2a17ea06c274ea844b49e999f6579d0454be36fc
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 10:14:55 +0530

    .

--- 22. Number Guessing/main.js
+++ 22. Number Guessing/main.js
@@ -2,4 +2,6 @@ const minNum = 1;
 const maxNum = 100;
 const ans = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-console.log(ans);
+// console.log(ans);
+let attempts = 0
+let guess;
