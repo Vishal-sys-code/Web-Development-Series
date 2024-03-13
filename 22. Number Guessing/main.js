f2fb22f9900735c72096b211f8765b7caecb5fa1
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 10:26:15 +0530

    Update main.js

--- 22. Number Guessing/main.js
+++ 22. Number Guessing/main.js
@@ -5,3 +5,28 @@ const ans = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 // console.log(ans);
 let attempts = 0
 let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
+    }
+    else{
+        attempts++;
+        if(guess < ans){
+            window.alert("TOO LOW! TRY AGAIN!!!");
+        }
+        else if(guess > ans){
+            window.alert("TOO HIGH! TRY AGAIN!!!");
+        }
+        else {
+            window.alert(`CORRECT!!! The answer was ${ans}. It tooks you ${attempts} to complete this procedure`);
+            running = false;
+        }
+    }
+}
