d44c3fe402ed02eed4150fb4b29b70131b1f5c9f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 18:06:13 +0530

    Update main.js

--- 26. Arrays/main.js
+++ 26. Arrays/main.js
@@ -21,37 +21,47 @@ undefined
 
 // ----------------------------------------------------------------------------
 
+console.log("----------------------------");
 fruits[3] = "coconut";
 console.log(fruits);
 
+console.log("----------------------------");
 fruits.pop();
 console.log(fruits);
 
+console.log("----------------------------");
 fruits.unshift("mango");
 console.log(fruits);
 
+console.log("----------------------------");
 fruits.shift();
 console.log(fruits);
 
+console.log("----------------------------");
 let len_fruits = fruits.length;
 console.log(len_fruits);
 
+console.log("----------------------------");
 let index = fruits.indexOf("banana");
 console.log(index);
 
+console.log("----------------------------");
 for(let i = 0; i < len_fruits; i++){
     console.log(fruits[i]);
 }
 
+console.log("----------------------------");
 for(let fruit of fruits){
     console.log(fruit);
 }
 
+console.log("----------------------------");
 fruits.sort();
 for(let fruit of fruits){
     console.log(fruit);
 }
 
+console.log("----------------------------");
 fruits.reverse();
 for(let fruit of fruits){
     console.log(fruit);
