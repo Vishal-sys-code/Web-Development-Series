d1989329720e2b88314498d2d4a53d644a58d5dc
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 17:31:52 +0530

    Create main.js

---
+++ 26. Arrays/main.js
@@ -0,0 +1,11 @@
+/* 
+Arrays: A variable like structures that can hold more than 1 value
+*/
+
+let fruits = ["apple", "orange", "banana"];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
