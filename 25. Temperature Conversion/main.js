deef22628869d5fa6753fe37f1102963d0bbf10f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-13 15:51:44 +0530

    Update main.js

--- 25. Temperature Conversion/main.js
+++ 25. Temperature Conversion/main.js
@@ -1,3 +1,8 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const myResult = document.getElementById("myResult");
+
 function convert(){
-    
+
 }
