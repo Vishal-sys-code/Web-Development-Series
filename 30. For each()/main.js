acbd60d88e0d814863646c6b0b8d20fa75c242c3
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-14 16:40:49 +0530

    Update main.js

--- 30. For each()/main.js
+++ 30. For each()/main.js
@@ -1,3 +1,27 @@
 /*
- 
+forEach(): Method is used to iterate over the elements of an array and apply a specified function(callback) to each element.
+
+array.forEach(callback)
+element, index, array are provided.
 */
+
+let numbers = [1, 2, 3, 4, 5];
+
+function double(element, index, array){
+    array[index] = element * 2;
+}
+
+function triple(element, index, array){
+    array[index] = element * 3;
+}
+
+function square(element, index, array){
+    array[index] = Math.pow(element, 2);
+}
+
+function display(element){
+    console.log(element);
+}
+
+numbers.forEach(double);
+numbers.forEach(display);
