3be134356ffddcf2cb5da0a0d36d6293f39915ce
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-14 10:21:01 +0530

    Create main.js

---
+++ 29. CallBacks/main.js
@@ -0,0 +1,8 @@
+/* 
+Callback: A function that is passed as an argument to another function.
+
+Used to handle Asynchronous operations: 
+1. Reading a file
+2. Network Requests
+3. Interacting with databases
+*/
