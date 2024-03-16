b554f3b1719671b35627976b0d487c22d68a448b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-16 09:43:10 +0530

    Update main.js

--- 30. For each()/main.js
+++ 30. For each()/main.js
@@ -26,7 +26,22 @@ function display(element){
 numbers.forEach(double);
 numbers.forEach(display);
 
-let fruits = [];
-function display(){
-     
+let fruits = ["apPle", "orAnge", "banAna", "coconUt"];
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
 }
+
+function lowerCase(element, index, array){
+    array[index] = element.toLowerCase();
+}
+
+function display_1(element){
+    console.log(element);
+}
+
+fruits.forEach(display);
+fruits.forEach(upperCase);
+fruits.forEach(display);
+fruits.forEach(lowerCase);
+fruits.forEach(display);
