76d4f8042ff787ef789a7496b8b131cc12124e6e
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-16 13:05:51 +0530

    Update main.js

--- 32. filter()/main.js
+++ 32. filter()/main.js
@@ -2,6 +2,7 @@
 .filter() = It creates a new array by filtering out elements
 */
 
+/*
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 
 function isEven(element){
@@ -17,3 +18,26 @@ let oddNums = numbers.filter(isOdd);
 
 console.log(evenNums);
 console.log(oddNums);
+*/
+
+// ------------------------------------------------------------
+
+const ages = [16, 17, 18, 19, 20, 60];
+
+function isAdult(element){
+    return element >= 18;
+}
+function isChild(element){
+    return element < 18;
+}
+function isRetire(element){
+    return element >= 60;
+}
+
+const adults = ages.filter(isAdult);
+const childrens = ages.filter(isChild);
+const retirementAgePeople = ages.filter(isRetire);
+
+console.log("Adults: ", adults);
+console.log("Childrens: ", childrens);
+console.log("Retired People: ", retirementAgePeople);
