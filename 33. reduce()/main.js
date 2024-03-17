4791c42f2b781e825b2c63344e259efb26cb4d7b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-17 12:42:40 +0530

    Update main.js

--- 33. reduce()/main.js
+++ 33. reduce()/main.js
@@ -31,3 +31,18 @@ and so on...
 
 const total = prices.reduce(sum);
 console.log(`$${total.toFixed(2)}`);
+
+// ------------------------------------------------------------
+
+const grades = [75, 50, 90, 60, 65, 80, 95];
+function getMax(accumulator, element){
+    return Math.max(accumulator, element);
+}
+function getMin(accumulator, element){
+    return Math.min(accumulator, element);
+}
+
+const maximum = grades.reduce(getMax);
+console.log(`The highest grade is ${maximum}`); 
+const minimum = grades.reduce(getMin);
+console.log(`The lowest grade is ${minimum}`);
