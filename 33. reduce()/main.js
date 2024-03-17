1ed568361526fbb5c56f34a6ca338aefc734de6b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-17 12:38:32 +0530

    Update main.js

--- 33. reduce()/main.js
+++ 33. reduce()/main.js
@@ -8,6 +8,26 @@ function sum(accumulator, element){
     return accumulator + element;
 }
 
+// we can also write the sum function as:
+/*
+function sum(previous, next){
+    return previous + next;
+}
+[5, 130, 10, 25, 15, 20]
+
+First Process:
+accumulator = 5
+next = 130
+sum = 135
+
+Second Process:
+accumulator = 135
+next = 10
+sum = 145
+
+and so on...
+
+*/
 
 const total = prices.reduce(sum);
 console.log(`$${total.toFixed(2)}`);
