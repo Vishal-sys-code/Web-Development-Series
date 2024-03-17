e21035c7e0f83b1dcce8c748ef6345acefab7b3e
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-17 12:50:38 +0530

    Update main.js

--- 34. Functions Expressions/main.js
+++ 34. Functions Expressions/main.js
@@ -3,3 +3,14 @@ Function Declaration: Define a resuable block of code that performs a specific t
 
 Function Expression: A way to define as value or variables.
 */
+
+// FUNCTION DECLARATION
+function hello_1(){
+    console.log("Hello");
+}
+
+// FUNCTION EXPRESSION
+const hello_2 = function(){
+    console.log("Hello");
+}
+hello_2();
