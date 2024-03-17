4eac4ab59ce4cc323fc2726fc8f8b0194c7a2bf0
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-17 12:36:16 +0530

    Update main.js

--- 33. reduce()/main.js
+++ 33. reduce()/main.js
@@ -2,3 +2,12 @@
 .reduce(): It reduces the elements of an array to a single value.
 */
 
+const prices = [5, 130, 10, 25, 15, 20];
+
+function sum(accumulator, element){
+    return accumulator + element;
+}
+
+
+const total = prices.reduce(sum);
+console.log(`$${total.toFixed(2)}`);
