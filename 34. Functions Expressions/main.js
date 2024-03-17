b2b87962a20b5f18f807e4cd19a943072f360037
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-17 13:00:17 +0530

    Update main.js

--- 34. Functions Expressions/main.js
+++ 34. Functions Expressions/main.js
@@ -21,3 +21,24 @@ setTimeout(function(){
 }, 3000);
 
 // ---------------------------------------------------------
+
+// This is the function declaration
+/*
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+
+function square(element){
+    return Math.pow(element, 2);
+}
+console.log(squares);
+*/
+
+// This is the function expressions
+/*
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(function(element){
+    return Math.pow(element, 2);
+});
+
+console.log(squares);
+*/
