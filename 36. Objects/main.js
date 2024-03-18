b651db61d3067bbf694e36d771a721c6ce9cffed
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-18 14:57:25 +0530

    Update main.js

---
+++ 36. Objects/main.js
@@ -0,0 +1,5 @@
+/*
+Objects: A collection of related properties and/or methods.
+        Can represent the real world objects (people, products, places)
+        object = {key: value, function()}
+*/
