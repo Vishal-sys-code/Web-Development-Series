04cff9bc719f8b585076a95a8bffc603dcee298f
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-18 17:47:41 +0530

    Update main.js

--- 36. Objects/main.js
+++ 36. Objects/main.js
@@ -3,3 +3,10 @@ Objects: A collection of related properties and/or methods.
         Can represent the real world objects (people, products, places)
         object = {key: value, function()}
 */
+
+const person = {
+    firstName: "Spongebob",
+    lastName: "Sqaurepants",
+    age: 30,
+    isEmployed: true,
+}
