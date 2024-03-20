e14f86e158a6c6b7aecac726226eb2f79ef07437
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-20 16:20:11 +0530

    Update main.js

--- 36. Objects/main.js
+++ 36. Objects/main.js
@@ -10,8 +10,19 @@ const person1 = {
     age: 30,
     isEmployed: true,
 }
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+}
 
 console.log(person1.firstName);
 console.log(person1.lastName);
 console.log(person1.age);
 console.log(person1.isEmployed);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
